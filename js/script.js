af9b75f40d7c959eaf7ab9d008e523173df4fa99
Sher77 <zaripovsherzat@mail.ru>
2023-05-23 20:53:19 +0600

    feat: add header

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// Lang switch
+
+const langBtn = document.querySelector('.lang-picker__btn');
+const langBtnList = document.querySelectorAll('.lang-picker__lang');
+
+let open = false;
+let lang;
+
+langBtn.addEventListener('click', event => {
+  let target = event.currentTarget;
+
+  lang = target.querySelector('.lang-picker__text-lang').textContent;
+  
+  target.querySelector('.lang-picker__arrow').classList.toggle('open');
+  target.classList.toggle('active');
+  target.nextElementSibling.classList.toggle('visually-hidden');
+  
+  if(!open) {
+    langBtnList.forEach(btn => {
+      btn.tabIndex = 0;
+    })
+
+    open = true;
+  } else {
+    langBtnList.forEach(btn => {
+      btn.tabIndex = -1;
+    })
+    
+    open = false;
+  }
+});
+
+
+langBtnList.forEach(btn => {
+  btn.addEventListener('click', event => {
+    document.querySelector('.lang-picker__text-lang').innerHTML = event.currentTarget.textContent;
+    event.currentTarget.textContent = lang;
+    event.currentTarget.style.color = "#000";
+    lang = document.querySelector('.lang-picker__text-lang').textContent;
+  })
+})
